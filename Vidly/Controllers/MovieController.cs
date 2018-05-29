e85e54b78ee90a67aae7eae6555f06539e7148ba
Pratul Belokar (COMMS-EGM) <pbelo@wipro.com>
2018-05-29 13:34:43 +0530

    my vidly project

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie()
+            {
+                id = 1,
+                Name = "Shrek !"
+            };
+
+            RandomMovieViewModel newmovie = new RandomMovieViewModel() {
+                customers = new List<Customer> {
+                       new Customer(){ id =1, Name = "Customer1"},
+                       new Customer(){ id =2, Name = "Customer2"},
+                },
+                movie = movie
+            };
+
+            return View(newmovie);
+        }
+
+
+    }
+}
