ea677346c1c08feb4d672d9deffd6e6c4be3bad0
Pratul Belokar <pratul.belokar@gmail.com>
2019-09-16 02:21:20 +0530

    dbchanges

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,7 +9,8 @@ namespace VidlyNew.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        //public bool IsSubscribedToNewsletter { get; set; }
-        //public MembershipType membershiptype { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
